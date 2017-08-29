20466a83efa455f473e0275fc931ee8d6ec51d74
EnhabytoBMI <rakesh22717@gmail.com>
2017-08-29 14:09:19 +0530

    intial commit

---
+++ app/src/main/java/com/enhabyto/bmiapp/LoginSignupStartPage.java
@@ -0,0 +1,13 @@
+ package com.enhabyto.bmiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginSignupStartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_signup_start_page);
+    }
+}
