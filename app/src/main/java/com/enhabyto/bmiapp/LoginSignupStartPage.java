59536fe514caad70b59467f84259bd119ed0cd61
EnhabytoBMI <rakesh22717@gmail.com>
2017-08-30 17:57:58 +0530

    second commit

--- app/src/main/java/com/enhabyto/bmiapp/LoginSignupStartPage.java
+++ app/src/main/java/com/enhabyto/bmiapp/LoginSignupStartPage.java
@@ -1,13 +1,57 @@
  package com.enhabyto.bmiapp;
 
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginSignupStartPage extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
+ public class LoginSignupStartPage extends AppCompatActivity {
+
+     EditText email, password;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            getSupportActionBar().hide();
+            Window window = getWindow();
+            //change notification color
+            // clear FLAG_TRANSLUCENT_STATUS flag:
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            //now change color
+            window.setStatusBarColor(ContextCompat.getColor(LoginSignupStartPage.this,R.color.loginnotificationbarcolor));
+        }
+        catch (NullPointerException e){
+            Toast.makeText(this, "Null Pointer Exception", Toast.LENGTH_SHORT).show();
+        }
+
         setContentView(R.layout.activity_login_signup_start_page);
+
+        email=(EditText)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.pass);
+
+
     }
+
+
 }
